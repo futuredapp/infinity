8a3b1a61e8d1ce2f2a25e5c902d242e88eb39aca
Lukas -skywall- Sztefek <lukas.sztefek@thefuntasty.com>
2016-08-16 08:53:03 +0200

    add missing @Retention annotations

--- infinity/src/main/java/com/thefuntasty/infinity/InfinityConstant.java
+++ infinity/src/main/java/com/thefuntasty/infinity/InfinityConstant.java
@@ -2,17 +2,22 @@ package com.thefuntasty.infinity;
 
 import android.support.annotation.IntDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class InfinityConstant {
 	public static final int IDLE = 0;
 	public static final int LOADING = 1;
 	public static final int FINISHED = 2;
 
+	@Retention(RetentionPolicy.SOURCE)
 	@IntDef({IDLE, LOADING, FINISHED})
 	public @interface Status {}
 
 	public static final int FIRST_PAGE = 0;
 	public static final int NEXT_PAGE = 1;
 
+	@Retention(RetentionPolicy.SOURCE)
 	@IntDef({FIRST_PAGE, NEXT_PAGE})
 	public @interface Part {}
 }
